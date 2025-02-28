7fa46d87a2e3ff949b0469b88d17368541b33462
Sarah Townson <26084137+Isioviel@users.noreply.github.com>
2025-02-28 17:25:49 +0000

    updates

---
+++ js/toggle.js
@@ -0,0 +1,14 @@
+// https://www.digitalocean.com/community/tutorials/css-collapsible
+
+let myLabels = document.querySelectorAll('.lbl-toggle');
+
+Array.from(myLabels).forEach(label => {
+  label.addEventListener('keydown', e => {
+    // 32 === spacebar
+    // 13 === enter
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
+      label.click();
+    };
+  });
+});
